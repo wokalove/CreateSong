9c8177fb1cba2280ce155a0136d21b22adcefa2f
wokalove <oladuda1999@gmail.com>
2021-08-16 22:04:09 +0200

    setup flask, api configuration

--- Api.py
+++ Api.py
@@ -1,10 +1,13 @@
 from flask import Flask
-from flask_restful import Resource, Api, reqparse
-import pandas as pd
-import ast
 
 app = Flask(__name__)
-api = Api(app)
+
+@app.route("/")
+def hello():
+  return "Hello World!"
+
+if __name__ == "__main__":
+  app.run()
 
 #https://www.stanleyulili.com/powershell/solution-to-running-scripts-is-disabled-on-this-system-error-on-powershell/
 #https://www.twilio.com/docs/usage/tutorials/how-to-set-up-your-python-and-flask-development-environment
